4200e3418552686bb6bc8037d18fbab1f224c8eb
andreasskovaagaard <eaaasaa@students.eaaa.dk>
2021-11-02 10:58:02 +0100

    crud added

---
+++ blazor/miniprojekt/miniprojekt/Client/Shared/Booking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace miniprojekt.Client.Shared
+{
+    [BsonIgnoreExtraElements]
+    public class Booking 
+    {
+        
+        private DateTime startdate { get; set; }
+        private DateTime slutdate { get; set; }
+        public int antal_pers { get; set; }
+        private string shelter_id { get; set; }
+        private string bruger_id { get; set; }
+        public int booking_id { get; set; }
+
+
+
+        public Booking( DateTime Startdate, DateTime Slutdate, int Antal_pers, string Shelter_id, string Bruger_id)
+        {
+            startdate = Startdate;
+            slutdate = Slutdate;
+            antal_pers = Antal_pers;
+            shelter_id = Shelter_id;
+            bruger_id = Bruger_id;
+        }
+
+
+
+
+    }
+}
---
+++ blazor/miniprojekt/miniprojekt/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using miniprojekt.Client.Shared;
+using miniprojekt.Shared;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace miniprojekt.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private IMongoCollection<shelter> _sheltercollection;
+        private IMongoCollection<Booking> _bookingcollection;
+
+        public WeatherForecastController(IMongoClient client)
+        {
+            var database = client.GetDatabase("shelterdb");
+            _sheltercollection = database.GetCollection<shelter>("shelter");
+            _bookingcollection = database.GetCollection<Booking>("booking");
+
+        }
+
+        [HttpGet]
+        public IEnumerable<shelter> Get()
+        {
+            return _sheltercollection.Find(s => s.status == "Gældende / Vedtaget").ToList();
+        }
+
+        [HttpPost]
+        public string Post(Booking booking)
+        {
+            int antalBooking = _bookingcollection.AsQueryable().Count();
+            booking.booking_id = antalBooking + 1;
+            _bookingcollection.InsertOneAsync(booking);
+
+            return "Added Succesfully";
+        }
+
+        [HttpPut]
+        public string Put(shelter shel)
+        {
+            var filter = Builders<Booking>.Filter.Eq("shelter_id", shel._id);
+            var update = Builders<Booking>.Update.Set("status", shel.status);
+
+            _bookingcollection.UpdateOne(filter, update);
+
+            return "Update Succesfully";
+        }
+
+        [HttpDelete]
+        public string Delete(shelter shel)
+        {
+            var filter = Builders<Booking>.Filter.Eq("shelter_id", shel._id);
+            var update = Builders<Booking>.Update.Set("status", shel.status);
+
+            _bookingcollection.UpdateOne(filter, update);
+
+            return "Update Succesfully";
+        }
+    }
+}
